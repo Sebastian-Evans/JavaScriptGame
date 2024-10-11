6bbafef103220e0b19ced22b9e5867021d4b627f
Sebastian-Evans <ianevans1992@hotmail.com>
2024-10-11 14:21:39 -0400

    Updated project 2

--- js/form.js
+++ js/form.js
@@ -1,5 +1,26 @@
+const gamespaceWidth = $("#gamespace").width();
+    
+const gamespaceHeight = $("#gamespace").height();
+
+
+let randomX = Math.floor(Math.random() * gamespaceWidth);
+
+let randomY = Math.floor(Math.random() * gamespaceHeight);
+
+alert(`The random x location is ${randomX}`);
+
+alert(`The random x location is ${randomY}`);
+
+let userScore = 0;
+
+function increaseScore(){
+    userScore++;
+    $("#score").text(userScore);
+}
+
 window.addEventListener('load', (event) =>{
 
+    
 
     let userName = window.prompt("What is your name?");
 
@@ -8,6 +29,25 @@ window.addEventListener('load', (event) =>{
 
     let pTag = document.getElementById("welcome");
     pTag.innerHTML = message;
+    
+    
 
-   
+    $("#timer").append("<span>30 seconds left</span>");
+    $("#start_button").on("click",function(){
+        alert("button has been clicked");
     });
+    $("#start_button").css({
+        "width" : "90px",
+        "height": "50px",
+        "font-size" : "22px",
+        "background-color": "#7cfc00"
+    });
+    });
+
+    $(document).ready(function(){
+        $("#gamespace").prepend($("<img>",{src:"img/soccerBall.png", alt:"picture of a soccer ball", class:"soccerBall"}));
+
+        increaseScore();
+       
+        });
+        
